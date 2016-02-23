04b2d1823931222e6792c3753acbffcfcb8c7e9b
lundgren84 <lundgren84@hotmail.se>
2016-02-23 11:58:07 +0100

    kk

--- Extra exercises 2/5/Program.cs
+++ Extra exercises 2/5/Program.cs
@@ -21,7 +21,7 @@ namespace _5
                 {
                     soon = DateTime.Now.AddMilliseconds(150);
                     
-                    dvektor.PrintArray(arr);
+                    dvektor.PrintArray(arr); // Här ska du skriva!"!
                 }
             }
            
